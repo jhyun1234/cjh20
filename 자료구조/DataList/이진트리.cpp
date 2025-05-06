4b14ea7b036026d2c029a44a43b00147ca8c90f4
내 이름 <anjyounl24@gmail.com>
2025-05-06 22:30:42 +0900

    2025 05 06

--- 자료구조/DataList/이진트리.cpp
+++ 자료구조/DataList/이진트리.cpp
@@ -3,96 +3,242 @@
 using namespace std;
 // 노드 구조체 정의
 struct Node {
-    int data;
-    Node* left;
-    Node* right;
+    int data; // 노드가 저장하는 데이터 값
+    Node* left; // 왼쪽 자식 노드를 가리키는 포인터
+    Node* right; // 오른쪽 자식 노드를 가리키는 포인터
 
-   
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+    // 소멸자: 노드 메모리 해제 확인용
+    ~Node() { cout << "Node " << data << " deleted" << endl; }
 };
 
 // 이진 탐색 트리 클래스 정의
 class BST
 {
 private:
-    Node* root;
+    Node* root; // 트리의 루트 노드를 가리키는 포인터
 
-public:
-    Node* searchNode(Node* root, int key) // 반복적인 탐색
+    // 노드 삽입 함수
+    // node : 현재 탐색 중인 서브트리의 루트
+    // value : 삽입할 값
+    // 반환값 : 삽입 후 변경된 서브트리의 루트 노드
+    Node* insertNode(Node* node, int value)
     {
-        while (root != NULL)
+        if (node == nullptr)
         {
-            if (root->data == key) return root;
-            else if (root->data > key)
-            {
-                root = root->left;
-            }
-            else
-            {
-                root = root->right;
-            }
+            cout << "노드 삽입 : " << value << endl;
+            return new Node(value); // 새 노드를 생성하여 반환
         }
-        return NULL;
+
+        // 현재 노드와 값 비교하여 이동
+        if (value < node->data)
+        {
+            // 값이 현재 노드보다 작으면 왼쪽 서브트리로 이동 및 삽입
+            node->left = insertNode(node->left, value);
+        }
+        else if (value > node->data)
+        {
+            // 값이 현재 노드보다 크면 오른쪽 서브트리로 이동 및 삽입
+            node->right = insertNode(node->right, value);
+        }
+        // else value == node->data 중복된 값은 삽입하지 않고 무시
+        return node; // 변경되지 않은 현재 노드 반환
     }
-    Node* searchNode(Node* root, int key) // 순환적인 탐색
+
+    // 노드 삭제 함수
+    // node : 현재 탐색 중인 서브트리의 루트
+    // value : 삭제할 값
+    // 반환값 : 삭제 후 변경된 서브트리의 루트 노드
+
+    Node* deleteNode(Node* node, int value)
     {
-        if (root == NULL) return NULL;
-        if (root->data == key) return root;
-        else if (root->data > key)
-            return searchNode(root->left, key);
+        if (node == nullptr)
+        {
+            return nullptr;
+        }
+
+        // 현재 노드와 값 비교하여 삭제할 노드 탐색
+        if (value < node->data)
+        {
+            // 삭제할 값이 현재 노드보다 작으면 왼쪽 서브트리에서 재귀 호출
+            node->left = deleteNode(node->left, value);
+        }
+        else if (value > node->data)
+        {
+            // 삭제할 값이 현재 노드보다 크면 오른쪽 서브트리에서 재귀 호출
+            node->right = deleteNode(node->right, value);
+        }
         else
-            return searchNode(root->right, key);
+        {
+            // 삭제할 노드를 찾았을 때 value == node -> data;
+
+            // 자식 노드가 없거나 하나만 있는 경우
+            if (node->left == nullptr)
+            {
+                // 왼쪽 자식이 없으면 오른쪽 자식을 반환
+                Node* temp = node->right;
+                cout << "노드 삭제" << node->data << endl;
+                delete node;
+                return temp;
+            }
+            else if (node->right == nullptr)
+            {
+                Node* temp = node->left;
+                cout << "노드 삭제" << node->data << endl;
+                delete node;
+                return temp;
+            }
+
+            // 자식 노드가 두 개 있는 경우
+            // 오른쪽 서브트리에서 가장 작은 값을 찾아서 대체
+            Node* temp = MinNode(node->right);
+
+            // 가장 작은 값의 값을 현재 노드에 복사
+            cout << "노드 삭제 (2개의 자식)" << node->data << " " << temp->data << endl;
+            node->data = temp->data;
+            node->right = deleteNode(node->right, temp->data);
+        }
+        return node; // 변경되지 않은 현재 노드 반환
     }
 
-    void insertNode(Node** root, int key)
+    // 서브트리에서 가장 작은 값(가장 왼쪽 노드)을 가진 노드를 찾는 함수
+    // node: 탐색을 시작할 서브트리의 루트
+    // 반환값: 가장 작은 값을 가진 노드 포인터 
+    Node* MinNode(Node* node)
     {
-        Node* parentNode = NULL, * currentNode, * newNode;
-        currentNode = *root;
-        // 키가 삽입되어야 할 위치를 반복적으로 찾는 과정
-        while (currentNode != NULL)
+        Node* current = node;
+        // 가장 왼쪽 노드까지 이동
+        while (current && current->left != nullptr)
         {
-            if (currentNode->data == key)
-                return;
-            parentNode = currentNode;
-            if (currentNode->data > key)
-            {
-                currentNode = currentNode->left;
-            }
-            else
-            {
-                currentNode = currentNode->right;
-            }
+            current = current->left;
+        }
+        return current;
+    }
+    // 서브트리에서 특정 값을 가진 노드를 찾는 함수
+    // node: 현재 탐색 중인 서브트리의 루트
+    // value: 찾으려는 값
+    // 반환값: 값을 가진 노드 포인터 
+    Node* searchNode(Node* node, int value)
+    {
+        if (node == nullptr || node->data == value)
+        {
+            return node;
+        }
+        // 현재 노드 값과 비교하여 왼쪽 또는 오른쪽으로 이동
+        if (value < node->data)
+        {
+            return searchNode(node->left, value);
+        }
+        else
+        {
+            return searchNode(node->right, value);
         }
 
-        newNode = new Node;
-        if (newNode == NULL)
+    }
+    // 중위 순회 헬퍼 함수
+    // node : 현재 방문 중인 노드
+    void inorderTrav(Node* node)
+    {
+        if (node == nullptr)
         {
             return;
         }
-        newNode->data = key;
-        newNode->left = newNode->right = NULL;
+        inorderTrav(node->left);
+        cout << node->data << " ";
+        inorderTrav(node->right);
+    }
 
-        if (parentNode != NULL)
+
+public:
+
+    // 트리 초기화
+    BST() : root(nullptr)
+    {
+
+    }
+    ~BST() {}
+
+    // 노드 삽입
+    void insert(int value)
+    {
+        root = insertNode(root, value); // 루트 노드부터 재귀 삽입 시작
+    }
+    // 노드 삭제
+    void deleteNode(int value)
+    {
+        root = deleteNode(root, value); // 루트 노드부터 재귀 삭제 시작
+    }
+
+    // 노드 탐색
+    // value : 찾으려는 값
+    // 반환값 : 값이 존재하면 true, 없으면 false
+    bool search(int value)
+    {
+        Node* found = searchNode(root, value);
+        if (found)
         {
-            if (parentNode->data > key)
-            {
-                parentNode->left = newNode;
-            }
-            else
-            {
-                parentNode->right = newNode;
-            }
-
+            cout << "값 " << value << " 찾음 " << endl;
+            return true;
         }
         else
         {
-            *root = newNode;
+            cout << "값 " << value << " 못찾음 " << endl;
+            return false;
         }
     }
+
+    // 중위 순회
+    void inorder()
+    {
+        cout << "BST inorder";
+        inorderTrav(root);
+        cout << endl;
+    }
+
+    // 추가 : 트리가 비어있는지 확인하는 함수
+    bool isEmpty()
+    {
+        return root == nullptr;
+    }
+
 };
 
-// 예시 사용법
-int main() {
-    
+
+int main()
+{
+
+    BST bst;
+    cout << "노드 삽입" << endl;
+    bst.insert(30);
+    bst.insert(40);
+    bst.insert(20);
+    bst.insert(10);
+    bst.insert(25);
+    bst.insert(35);
+
+    cout << endl;
+    bst.inorder();
+
+    cout << "노드 탐색" << endl;
+    bst.search(40);
+    bst.search(50);
+    bst.search(30);
+
+    cout << "노드 삭제 20" << endl;
+    bst.deleteNode(20);
+    bst.inorder();
+
+    cout << "노드 삭제 25" << endl;
+    bst.deleteNode(25);
+    bst.inorder();
+
+    cout << "노드 삭제 35" << endl;
+    bst.deleteNode(35);
+    bst.inorder();
+
+
+
 
     return 0;
 }
+
